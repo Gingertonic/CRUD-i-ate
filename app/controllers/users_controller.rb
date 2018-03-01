b2b27513751f323d440f7975e562863be161624e
petitlapin86 <skinnerpa86@yahoo.com>
2018-03-01 15:52:14 -0500

    working on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
 
-end 
+
+  get '/signup' do
+     if logged_in?
+       flash[:existing_user_error] = "You're already signed up and logged in"
+       direct to '/'
+     else
+       erb :'/users/signup'
+     end
+   end
+
+   post '/signup' do
+    if blank_params?("user")
+      flash[:signup_error] = "One or more fields were left empty. Please fill in all fields."
+      redirect to '/signup'
+    else
+      flash[:signup_success] = "Thanks for signing up!"
+      @user = User.create(params[:user])
+      session[:user_id] = @user.id
+      redirect to '/'
+    end
+  end
+
+end
