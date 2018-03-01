999613969538de5ef150fd79f4544e56258232a7
petitlapin86 <skinnerpa86@yahoo.com>
2018-03-01 15:54:01 -0500

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 
    post '/signup' do
     if blank_params?("user")
-      flash[:signup_error] = "One or more fields were left empty. Please fill in all fields."
+      flash[:signup_error] = "Uhoh! You left some fields empty. Please fill in all fields."
       redirect to '/signup'
     else
       flash[:signup_success] = "Thanks for signing up!"
@@ -22,4 +22,29 @@ class UsersController < ApplicationController
     end
   end
 
-end
+  get '/login' do
+      if logged_in?
+        redirect to '/'
+      else
+        erb :'/users/login'
+      end
+    end
+
+    post '/login' do
+      @user = User.find_by(username: params[:user][:username])
+      if @user && @user.authenticate(params[:user][:password])
+        flash[:login_success] = "Welcome, #{@user.username.capitalize}"
+        session[:user_id] = @user.id
+        redirect to '/'
+      else
+        flash[:login_error] = "The login or password entered is invalid"
+        redirect to '/login'
+      end
+    end
+
+    get '/logout' do
+      flash[:logout_success] = "Logged out successfully"
+      session.clear
+      redirect to '/'
+    end
+  end
