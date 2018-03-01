87117de8824ae12956659114d80035da8a8d780f
petitlapin86 <skinnerpa86@yahoo.com>
2018-03-01 15:29:36 -0500

    add CRUD methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,29 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+    set :views, 'app/views'
+    register Sinatra::Flash
+  end
+
   get '/' do
     @posts = Post.all
     erb(:'/posts/index')
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def blank_params?(hash_key)
+      params["#{hash_key}"].values.any?{|param| param.empty?}
+    end
+  end
 end
