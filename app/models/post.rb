2643edeb4f96a619a91d0535c8a4193d2701ac43
petitlapin86 <skinnerpa86@yahoo.com>
2018-03-01 15:56:59 -0500

    work on models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,30 @@ class Post < ActiveRecord::Base
 
 belongs_to :user
 
+@@default_categories = [
+  "funny",
+  "kids",
+  "gluttany",
+  "moldy"
+]
+
+def slug
+  self.title.parameterize
+end
+
+def self.find_by_slug(slug)
+  self.all.find {|instance| instance.slug == slug}
+end
+
+def self.default_categories
+  @@default_categories
+end
+
+# only shows current categories - not sure if useful but works
+#def self.all_categories
+#  self.all.map do |activity|
+#    activity.category
+#  end.uniq
+#end
+
 end
