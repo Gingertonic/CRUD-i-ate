5ef91c7b0dd0c2fd3315d17a86f8e56b7f55270b
petitlapin86 <skinnerpa86@yahoo.com>
2018-03-01 15:58:06 -0500

    working on models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_many :activities
-  
+  has_secure_password
+  validates_presence_of :username, :password
+
+def slug
+  self.username.parameterize
+end
+
+def self.find_by_slug(slug)
+  self.all.find {|instance| instance.slug == slug}
+end
 end
